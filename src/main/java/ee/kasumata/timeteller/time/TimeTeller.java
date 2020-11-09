3c3917d7b4e45f5c29896be8260c01df7ba86c56
Taikelenn <taikelenn@kasumata.ee>
2020-11-09 18:16:15 +0100

    Added an optional constructor argument for the time teller to facilitate testing

--- src/main/java/ee/kasumata/timeteller/time/TimeTeller.java
+++ src/main/java/ee/kasumata/timeteller/time/TimeTeller.java
@@ -2,8 +2,6 @@ package ee.kasumata.timeteller.time;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.time.format.DateTimeFormatter;
-
 public interface TimeTeller {
     @NotNull
     String getFormattedTime();
--- src/main/java/ee/kasumata/timeteller/time/TimeTellerImpl.java
+++ src/main/java/ee/kasumata/timeteller/time/TimeTellerImpl.java
@@ -2,25 +2,35 @@ package ee.kasumata.timeteller.time;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoUnit;
 
 public class TimeTellerImpl implements TimeTeller {
+    @NotNull
     private final DateTimeFormatter dateFormatter;
 
+    @NotNull
+    private final Clock clock;
+
     public TimeTellerImpl() {
+        this(Clock.systemDefaultZone());
+    }
+
+    public TimeTellerImpl(@NotNull Clock clock) {
         dateFormatter = new DateTimeFormatterBuilder()
                 .append(DateTimeFormatter.ISO_LOCAL_DATE)
                 .appendLiteral(' ')
                 .append(DateTimeFormatter.ISO_LOCAL_TIME)
                 .toFormatter();
+        this.clock = clock;
     }
 
     @Override
     public @NotNull String getFormattedTime() {
-        LocalDateTime currentTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS); // limit the precision to seconds, more detailed time is not very user-readable
+        LocalDateTime currentTime = LocalDateTime.now(clock).truncatedTo(ChronoUnit.SECONDS); // limit the precision to seconds, more detailed time is not very user-readable
         return currentTime.format(dateFormatter);
     }
 }
