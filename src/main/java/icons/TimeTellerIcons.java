1f29936e4d9088400d5af5c95d4a0290e0eecce4
Taikelenn <taikelenn@kasumata.ee>
2020-11-11 16:37:04 +0100

    Added an icon for the time telling action

---
+++ src/main/java/icons/TimeTellerIcons.java
@@ -0,0 +1,9 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface TimeTellerIcons {
+    Icon TellTimeIcon = IconLoader.getIcon("/META-INF/icons/tellTime.svg");
+}
